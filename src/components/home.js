014e069392f8cefdb468fdcf6709f9efdcc6cf71
PRAVEEN KUMAR <calltopraveen@gmail.com>
2022-04-27 16:02:01 +0530

    updates

--- src/components/home.js
+++ src/components/home.js
@@ -1,85 +1,10 @@
 import React from 'react';
-import { Card, Paper, Grid, Button, Stack, CardContent, Typography, Divider} from '@mui/material';
-import { makeStyles } from '@mui/styles';
+import { Paper, Grid, Button, Stack, Typography} from '@mui/material';
 import { createTheme } from "@mui/material/styles";
-import Page1 from './page1'
-
 
 const theme = createTheme();
 
-const useStyles = makeStyles(()=>({
-    root: {
-      //background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-      border: 0,
-      color: 'white',
-      width: "100vw",
-      top: 0,
-    },
-    page1: {
-        position: "relative",
-        top: "-120px"
-    },
-    lastName:{
-        paddingLeft: "90px",
-        color: "#000",
-        fontSize: "90px",
-        [theme.breakpoints.down('md')]: {
-            fontSize: "40px",
-            fontWeight: "bold",
-            paddingLeft: "20px",
-        },
-    },
-    heading1:{
-        position: 'absolute',
-        top: 150
-    },
-    para:{
-        [theme.breakpoints.down('md')]: {
-            position: 'inherit',
-            fontSize: "20px",
-            fontWeight: "bold",
-            //padding: "10px",
-            margin: "0px",
-            width: "100vw",
-        },
-        [theme.breakpoints.down('xl')]: {
-            display: 'none'
-        },
-        position: 'absolute',
-        top: 440,
-        backgroundColor: "#fff",
-        opacity: 0.8,
-        padding: "10px",
-        margin: "20px",
-        width: "40%"
-    },
-    line: {
-        borderWidth: "3px",
-        borderColor: "#B91224!important"
-    },
-    title: {
-        fontSize: "50px",
-        fontWeight: "bold",
-        color: "#fff!important",
-        [theme.breakpoints.down('md')]: {
-            fontSize: "16px",
-            fontWeight: "bold"
-        }
-    },
-    pt10: {
-        paddingTop: "10px",
-        textAlign: "justify"
-    },
-    para1: {
-        paddingTop: "10px",
-        textAlign: "justify",
-        color: "#000"
-    }
-
-}));
-
 const Home = (props) => {
-    const classes = useStyles();
     return (
         <React.Fragment>
             <Stack
